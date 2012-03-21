d570283a89778f08c086c879c10dd8c12ce6f579
YA Series Apps <yasappdev@gmail.com>
2012-03-21 02:35:47 -0500

    Nightly commit for 03/21
    Working Tabs
    Fixed version to 0.1

--- gen/com/shankarlabs/sms/R.java
+++ gen/com/shankarlabs/sms/R.java
@@ -77,27 +77,29 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int actionbar_compat_item_refresh=0x7f060003;
         public static final int actionbar_compat_item_refresh_progress=0x7f060002;
         public static final int actionbar_compat_title=0x7f060001;
-        public static final int backupnowbtn=0x7f060009;
-        public static final int backupprogress=0x7f060006;
+        public static final int backupnowbtn=0x7f06000a;
+        public static final int backupprogress=0x7f060007;
+        public static final int fragment_content=0x7f060005;
         public static final int menu_refresh=0x7f060004;
-        public static final int menu_search=0x7f060017;
-        public static final int menu_share=0x7f060018;
-        public static final int optionsrestore=0x7f060014;
-        public static final int optionswhat=0x7f06000e;
-        public static final int optionswhen=0x7f06000b;
-        public static final int optionswhere=0x7f060011;
-        public static final int restoreheadtxtview=0x7f060015;
-        public static final int restoresumtxtview=0x7f060016;
-        public static final int settings_fragment=0x7f060005;
-        public static final int statuscount=0x7f060008;
-        public static final int statustext=0x7f060007;
-        public static final int stopbackupbtn=0x7f06000a;
-        public static final int whatheadtxtview=0x7f06000f;
-        public static final int whatsumtxtview=0x7f060010;
-        public static final int whenheadtxtview=0x7f06000c;
-        public static final int whensumtxtview=0x7f06000d;
-        public static final int whereheadtxtview=0x7f060012;
-        public static final int wheresumtxtview=0x7f060013;
+        public static final int menu_search=0x7f060019;
+        public static final int menu_share=0x7f06001a;
+        public static final int optionsrestore=0x7f060015;
+        public static final int optionswhat=0x7f06000f;
+        public static final int optionswhen=0x7f06000c;
+        public static final int optionswhere=0x7f060012;
+        public static final int restoreheadtxtview=0x7f060016;
+        public static final int restoresumtxtview=0x7f060017;
+        public static final int settings_fragment=0x7f060006;
+        public static final int statuscount=0x7f060009;
+        public static final int statustext=0x7f060008;
+        public static final int stopbackupbtn=0x7f06000b;
+        public static final int viewsms_fragment=0x7f060018;
+        public static final int whatheadtxtview=0x7f060010;
+        public static final int whatsumtxtview=0x7f060011;
+        public static final int whenheadtxtview=0x7f06000d;
+        public static final int whensumtxtview=0x7f06000e;
+        public static final int whereheadtxtview=0x7f060013;
+        public static final int wheresumtxtview=0x7f060014;
     }
     public static final class layout {
         public static final int actionbar_compat=0x7f030000;
@@ -105,6 +107,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int main=0x7f030002;
         public static final int settings=0x7f030003;
         public static final int settings_fragment=0x7f030004;
+        public static final int viewsms=0x7f030005;
+        public static final int viewsms_fragment=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f090000;
--- src/com/shankarlabs/sms/SaveMySMSActivity.java
+++ src/com/shankarlabs/sms/SaveMySMSActivity.java
@@ -1,10 +1,18 @@
 package com.shankarlabs.sms;
 
 import com.example.android.actionbarcompat.ActionBarFragmentActivity;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.ActionBar.Tab;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class SaveMySMSActivity extends ActionBarFragmentActivity
@@ -14,7 +22,44 @@ public class SaveMySMSActivity extends ActionBarFragmentActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        //Add the two tabs on load
+        ActionBar actionBar = getActionBar();
+        
+        final String settingsTabTag = "settingsTab";
+        final String settingsTabTitle = "Settings";
+        final int settingsTabId = R.layout.settings_fragment;
+        actionBar.addTab(actionBar.newTab()
+                .setText(settingsTabTitle) // The Tab Title
+                .setTabListener(new TabListener(new TabFragment(settingsTabId, settingsTabTag))));
+        
+        final String viewSMSTabTag = "ViewSMSTab";
+        final String viewSMSTabTitle = "View SMS";
+        final int viewSMSTabId = R.layout.viewsms_fragment;
+        actionBar.addTab(actionBar.newTab()
+                .setText(viewSMSTabTitle) // The Tab Title
+                .setTabListener(new TabListener(new TabFragment(viewSMSTabId, viewSMSTabTag))));
+        
+        // Time to enable the tabs
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE); //, ActionBar.DISPLAY_SHOW_TITLE); // Don't disable anything
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
     }
+    
+	
+	public void onAddTab(View v)
+	{
+        final ActionBar bar = getActionBar();
+        final int tabCount = bar.getTabCount();
+        final String text = "Tab " + tabCount;
+        bar.addTab(bar.newTab()
+                .setText(text)
+                .setTabListener(new TabListener(new TabFragment(R.layout.settings_fragment, "Settings"))));
+    }
+
+    public void onRemoveTab(View v) {
+        final ActionBar bar = getActionBar();
+        bar.removeTabAt(bar.getTabCount() - 1);
+    }
+   
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,5 +99,61 @@ public class SaveMySMSActivity extends ActionBarFragmentActivity
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    /**
+     * A TabListener receives event callbacks from the action bar as tabs
+     * are deselected, selected, and reselected. A FragmentTransaction
+     * is provided to each of these callbacks; if any operations are added
+     * to it, it will be committed at the end of the full tab switch operation.
+     * This lets tab switches be atomic without the app needing to track
+     * the interactions between different tabs.
+     *
+     * NOTE: This is a very simple implementation that does not retain
+     * fragment state of the non-visible tabs across activity instances.
+     * Look at the FragmentTabs example for how to do a more complete
+     * implementation.
+     */
+    private class TabListener implements ActionBar.TabListener {
+        private TabFragment mTabFragment;
+
+        public TabListener(TabFragment fragment) {
+        	mTabFragment = fragment;
+        }
+
+        public void onTabSelected(Tab tab, FragmentTransaction ft) {
+            ft.add(R.id.fragment_content, mTabFragment, mTabFragment.getTabTag());
+        }
+
+        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+            ft.remove(mTabFragment);
+        }
+
+        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+            Toast.makeText(SaveMySMSActivity.this, "Reselected!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    
+    private class TabFragment extends Fragment
+    {
+    	private int mTabId;
+        private String mTabTag;
+
+        public TabFragment(int tabId, String tag) {
+        	mTabId = tabId;
+        	mTabTag = tag;
+        }
+
+        public String getTabTag() {
+            return mTabTag;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View fragmentView = inflater.inflate(mTabId, container, false);
+            return fragmentView;
+        }
+
     }
 }
